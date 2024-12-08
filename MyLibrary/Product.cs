2383dd7576511858a1e194bf43880ea18f21ffd4
JosephParraT <Josephpt2004@outlook.com>
2024-12-08 12:27:58 -0500

    Cambios My library and Product

---
+++ MyLibrary/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Product
+    {
+        public int Id;
+        public string Name;
+        public string UnitPrice;
+        public string UnitsInStock;
+    }
+}
