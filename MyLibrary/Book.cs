74c8f825ecf56e2c713bab6ba07acca1573e890d
JosephParraT <Josephpt2004@outlook.com>
2024-12-08 12:57:02 -0500

    Book

---
+++ MyLibrary/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace MyLibrary
+{
+    public class Book : Product
+    {
+        public string  Title { get; }
+        public Book(int id, string name, decimal UnitPrice, int unitInStock, string title) : base(id,name,UnitPrice,unitInStock)
+        {
+            Title = title;
+        }
+    }
+
+    public class Book2(int id, string name, decimal UnitPrice, int unitInStock, string title) : Product(id, name, UnitPrice, unitInStock)
+    {
+        public string Title => title;
+
+
+
+
+    }
+
+
+
+}
